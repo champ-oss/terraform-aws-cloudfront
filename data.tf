80653c8f75e3b4f2e5413f300d8398832cc43f6e
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-06-14 15:15:48 -0500

    setting iam policy back to normal

--- data.tf
+++ data.tf
@@ -1,36 +1,27 @@
 data "aws_iam_policy_document" "this" {
   statement {
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
     actions = ["s3:GetObject"]
     resources = [
       module.s3.arn,
       "${module.s3.arn}/*"
     ]
-    condition {
-      variable = "aws:SourceArn"
-      test     = "StringEquals"
-      values   = [aws_cloudfront_distribution.this.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.this.iam_arn]
     }
   }
 
   statement {
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
     actions = ["s3:ListBucket"]
     resources = [
       module.s3.arn,
       "${module.s3.arn}/*"
     ]
 
-    condition {
-      variable = "aws:SourceArn"
-      test     = "StringEquals"
-      values   = [aws_cloudfront_distribution.this.arn]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.this.iam_arn]
     }
   }
 }
