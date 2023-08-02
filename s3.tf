313fa83261a56b9cb1cd156d3dd52bed7f0c79c6
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-08-02 11:36:41 -0500

    updating s3 rel tag

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source  = "github.com/champ-oss/terraform-aws-s3.git?ref=7eb17035fc9de643796a670c90e0c621586f894d"
+  source  = "github.com/champ-oss/terraform-aws-s3.git?ref=v1.0.39-d4fa5e1"
   git     = substr(var.git, 0, 60)
   name    = var.name
   protect = var.protect
